b3141f0646e12ef08658c58ae8854d9ff35e8abb
MDrapala <drapalamathieu95@gmail.com>
2023-10-17 10:49:01 +0200

    firebase hosting init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  /* config options here */
+  images: {
+    loader: "akamai",
+    path: "https://qcnotes-6baac.web.app",
+    domains: [
+      "https://qcnotes-6baac.web.app",
+      "https://www.qcnotes-6baac.web.app",
+      "http://localhost:3000"
+    ]
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+}
 
 module.exports = nextConfig
